1e8687b53c38a3b62ced6984e6bb0b79b4aee0ef
Rafał Mytych <rfm@europe.com>
2020-07-29 18:36:35 +0200

    two new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './library/books/books.component';
+import { AuthorsComponent } from './library/authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
