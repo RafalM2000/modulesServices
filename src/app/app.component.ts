bac565d41f1293c2a5078027925797e317f31912
Rafał Mytych <rfm@europe.com>
2020-07-29 18:21:23 +0200

    blue print project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'modulesAndServices';
+  title1: string = 'Angular-In-Examples';
+  title2: string = 'Modules and Services';
 }
